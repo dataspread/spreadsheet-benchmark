eaf4becacb1e5d696ff41a64e51e58a2e3ae0007
srahman7 <srahman7@illinois.edu>
2020-05-03 23:59:56 -0500

    memory consumption automated

--- oot/data layout/memory.sh
+++ oot/data layout/memory.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-#open each file in libre office manually
-#TODO: automate the process
-pid=$(ps aux | grep soffice.bin | head -n 1 | awk '{print $2}') # get soffice process id
-pmap -x $pid | tail -n 1 | awk '{print $5}' #get memomry consumption
-
-
+array=( 10000 30000 50000 )
+for j in "${array[@]}" # for each file size
+do
+    for i in {1..10} # for 10 trials
+    do  
+        file = "/path/to/file" #file directory
+        extension = ".ods"
+        filepath = $file$j$extension #filename with path
+        libreoffice --calc "${filepath}" #open file
+        pid=$(ps aux | grep soffice.bin | head -n 1 | awk '{print $2}') # get soffice process id
+        pmap -x $pid | tail -n 1 | awk '{print $5}' #get memomry consumption
+        kill -9 $pid # close file
+    done
+done
